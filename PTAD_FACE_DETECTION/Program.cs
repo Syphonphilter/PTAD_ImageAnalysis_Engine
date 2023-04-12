9fbca298555979f364ad5895bfb4be16b66b925d
Syphonphilter <Syphonphilter@DESKTOP-CE92CDI>
2023-04-12 12:20:46 +0100

    Enhancements

--- PTAD_FACE_DETECTION/Program.cs
+++ PTAD_FACE_DETECTION/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using System.Drawing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
 using Microsoft.Azure.CognitiveServices.Vision.Face;
 using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 using OfficeOpenXml;
+using Image = SixLabors.ImageSharp.Image;
+using SixLabors.ImageSharp.Processing.Processors.Convolution;
 
 namespace FaceDetection
 {
@@ -18,6 +22,7 @@ namespace FaceDetection
         {
             // Replace with the path to your folder of headshot images
             string folderPath = @"C:\BESPhotos\TEST\";
+            string enfolderPath = @"C:\BESPhotos\EnhancedImages\";
 
             // Create an Excel package
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
@@ -31,10 +36,16 @@ namespace FaceDetection
             List<Tuple<string, string>> facesNotDetected = new List<Tuple<string, string>>();
             // Loop through each image file in the folder
             DirectoryInfo directory = new DirectoryInfo(folderPath);
+            DirectoryInfo enhcanceddirectory = new DirectoryInfo(enfolderPath);
             foreach (FileInfo file in directory.GetFiles("*.jpg"))
             {
+                string fileName = Path.GetFileNameWithoutExtension(file.Name);
+                EnchanceImage(fileName,false);
+            }
+            foreach (FileInfo filec in enhcanceddirectory.GetFiles("*.jpg"))
+            {
                 // Load the image file
-                using (Stream imageStream = File.OpenRead(file.FullName))
+                using (Stream imageStream = File.OpenRead(filec.FullName))
                 {
                     try
                     {
@@ -47,11 +58,17 @@ namespace FaceDetection
                             }).Result;
 
                         // Add the file name and face detection result to the Excel sheet
-                        string fileName = Path.GetFileNameWithoutExtension(file.Name);
+                        string fileName = Path.GetFileNameWithoutExtension(filec.Name);
+                      
                         if (faces.Count > 0)
                         {
                             double qualityScore = (1 - faces[0].FaceAttributes.Blur.Value) * (1 - faces[0].FaceAttributes.Exposure.Value);
+                       
                             facesDetected.Add(Tuple.Create(fileName, qualityScore.ToString()));
+                            if (qualityScore < 0.6)
+                            {
+                                EnchanceImage(fileName,true);
+                            }
 
                         }
                         else
@@ -91,6 +108,7 @@ namespace FaceDetection
                 {
                     worksheet.Cells[row, 1].Value = filename;
                     worksheet.Cells[row, 2].Value = "No";
+                    worksheet.Cells[row, 3].Value = filename.Item2;
                     row++;
                 }
 
@@ -101,5 +119,92 @@ namespace FaceDetection
             // Save the Excel package
            // package.SaveAs(new FileInfo(@"C:\Users\Syphonphilter\Documents\PTAD DOCS\FaceDetectionResults.xlsx"));
         }
+
+
+        public static async void EnchanceImage(string fileName, bool?secondphase)
+        {
+            // Specify the input and output folders
+            string folderPath = "";
+            if (secondphase== true)
+            {
+                 folderPath = @"C:\BESPhotos\EnhancedImages\";
+            }
+            else
+            {
+                 folderPath = @"C:\BESPhotos\TEST\";
+            }
+           
+
+            string outputFolder = @"C:\BESPhotos\EnhancedImages\";
+            string secondoutputFolder = @"C:\BESPhotos\ReEnhancedImages\";
+           
+            // Create the output folder if it doesn't exist
+            if (!Directory.Exists(outputFolder))
+            {
+                Directory.CreateDirectory(outputFolder);
+            } if (!Directory.Exists(secondoutputFolder))
+            {
+                Directory.CreateDirectory(secondoutputFolder);
+            }
+
+            // Get the list of image files in the input folder
+
+
+
+
+            try
+            {
+                // Load the image from the file path
+                var  extension = ".jpg";
+                
+                using (Stream imageStream = File.OpenRead(folderPath + fileName+extension))
+                {
+                   using var image = Image.Load(folderPath + fileName + ".jpg");
+                    // Detect faces in the image
+
+                    if (secondphase == true)
+                    {
+                        image.Mutate(x => x.GaussianSharpen(1.1f));
+
+                        // Apply a brightness filter to the image
+
+                        image.Mutate(x => x.Brightness((float)(0.2) * 5));
+                    }
+                    else
+                    {
+                        image.Mutate(x => x.GaussianSharpen(1.5f));
+
+                        // Apply a brightness filter to the image
+
+                        image.Mutate(x => x.Brightness((float)(0.4) * 5));
+                    }
+                    string filePath = outputFolder+fileName + ".jpg";
+
+                    if (secondphase == true)
+                    {
+                        string outputFilePath = Path.Combine(secondoutputFolder, Path.GetFileNameWithoutExtension(fileName) + ".jpg");
+                        image.Save(outputFilePath);
+                    }
+                    else
+                    {
+                        string outputFilePath = Path.Combine(outputFolder, Path.GetFileNameWithoutExtension(fileName) + ".jpg");
+                        image.Save(outputFilePath);
+                    }
+                }
+
+                // Save the enhanced image to the output folder
+             
+
+            }
+            
+            catch (Exception ex)
+            {
+                // Log any exceptions and continue to the next image
+                Console.WriteLine($"Error processing image {fileName}: {ex.Message}");
+                
+            }
+            }
+
+        }
     }
-}
+
